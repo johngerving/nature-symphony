ae7daebfc522cac8555ccd673ac9366368805a96
johngerving <gervingjohn@gmail.com>
2025-03-08 10:25:26 -0800

    Saving form

---
+++ src/routes/auth/login/login-form.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { formSchema, type FormSchema } from './schema';
+	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+
+	let { data }: { data: { form: SuperValidated<Infer<FormSchema>> } } = $props();
+
+	const form = superForm(data.form, {
+		validators: zodClient(formSchema)
+	});
+
+	const { form: formData, enhance } = form;
+</script>
+
+<form method="POST" use:enhance>
+	<Form.Field {form} name="email">
+		<Form.Control>
+			{#snippet children({ props })}
+				<Form.Label>Email</Form.Label>
+				<Input {...props} bind:value={$formData.email} />
+			{/snippet}
+		</Form.Control>
+		<Form.Description>This is your public display name.</Form.Description>
+		<Form.FieldErrors />
+	</Form.Field>
+	<Form.Button>Submit</Form.Button>
+</form>
