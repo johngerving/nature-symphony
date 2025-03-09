7da3281a027e9ad615614c4f1bcc3896e449fe7a
johngerving <gervingjohn@gmail.com>
2025-03-09 12:01:22 -0700

    Get favorites from database

---
+++ src/lib/types/database.ts
@@ -0,0 +1,3 @@
+import type { Database } from '../../../database.types';
+
+export type Favorite = Database['public']['Tables']['favorites']['Row'];
--- src/routes/(authenticated)/favorites/+page.server.ts
+++ src/routes/(authenticated)/favorites/+page.server.ts
@@ -1,5 +1,16 @@
+import type { Favorite } from '$lib/types/database';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async (event) => {
-	return {};
+export const load: PageServerLoad = async ({ locals: { supabase } }) => {
+	const { data: favorites } = (await supabase
+		.from('favorites')
+		.select('id')
+		.order('created_at')) as {
+		data: Favorite[];
+	};
+
+	console.log(favorites);
+	return {
+		favorites: favorites ?? []
+	};
 };
