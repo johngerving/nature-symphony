de8b6cf37d9243cb38815d3176115e80473c5967
johngerving <gervingjohn@gmail.com>
2025-03-09 11:09:15 -0700

    Set up Supabase db

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -7,25 +7,6 @@
 	let updating = $state(false);
 </script>
 
-<header>
-	<nav>
-		<a href="/">Home</a>
-	</nav>
-	<form
-		method="POST"
-		action="/auth/logout"
-		use:enhance={() => {
-			updating = true;
-
-			return async ({ update }) => {
-				await update();
-				updating = false;
-			};
-		}}
-	>
-		<Button type="submit">Log Out</Button>
-	</form>
-</header>
 <main>
 	{@render children()}
 </main>
