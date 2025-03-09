2f19db7b6b5cef69e1cbcef2a92a7ab3169baa56
Stenpai <145399052+Stenpai@users.noreply.github.com>
2025-03-08 17:23:40 -0800

    searchbar added, needs to be fixed for search capability and because it doesn't stick

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,83 @@
 	});
 </script>
 
+<header>
+
+    <div class="search-bar">
+
+      <input id="search" type="search" placeholder="&#x1F50D; Start typing to search..." />
+
+    </div>
+
+</header>
+
+<main class="container">
+
+    <div class="search-display"></div>
+
+    
+
+    <div class="posts-container"></div>
+
+</main>
+
+
+<style>
+div.absolute {
+	position: absolute;
+	top: 180px;
+	right: 0;
+	width: 200px;
+	height: 100px;
+	border: 3px solid #4a4e44;
+  }
+  header {
+
+position: sticky;
+
+top: -54px; /* value to scroll past the logo so just the search bar is sticky */
+
+z-index: 2;
+
+}
+
+.search-bar {
+
+display: flex;
+
+justify-content: center;
+
+padding: 24px;
+
+}
+
+.search-bar input {
+
+width: 50%;
+
+min-width: 300px;
+
+padding: 12px 24px;
+
+border-radius: 24px;
+
+font-size: 16px;
+
+border: 0px;
+
+outline: none;
+
+}
+
+.search-bar [list]::-webkit-list-button,
+
+.search-bar [list]::-webkit-calendar-picker-indicator {
+
+display: none !important;
+
+}
+
+</style>
 <Navbar {user}/>
 
 {@render children()}
