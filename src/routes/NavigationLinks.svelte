e2e66a7abdcdfd2b96eb792862df2b1dca598b01
JC <cabraljaron03@gmail.com>
2025-03-08 19:48:57 -0800

    added dashboard to navigate map/forest/gallery

---
+++ src/routes/NavigationLinks.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<style>
+  .nav-container {
+    position: absolute;
+    top: 70px;
+    right: 10px;
+    display: flex;
+    flex-direction: column;
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 10px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .nav-link {
+    margin: 5px 0;
+    text-decoration: none;
+    color: #333;
+    font-weight: bold;
+  }
+
+  .nav-link:hover {
+    color: #007BFF;
+  }
+</style>
+
+<div class="nav-container">
+  <a class="nav-link" href="/map">Map</a>
+  <a class="nav-link" href="/forest">Forest</a>
+  <a class="nav-link" href="/gallery">Gallery</a>
+</div>
