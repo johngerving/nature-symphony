a516688c125197507abc9c2dca337a52c1ac07e0
johngerving <gervingjohn@gmail.com>
2025-03-09 15:22:14 -0700

    Fix links

--- src/lib/components/ui/navbar/navbar.svelte
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -1,53 +1,31 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button/index.js";
-    import * as Avatar from "$lib/components/ui/avatar/index.js";
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Avatar from '$lib/components/ui/avatar/index.js';
 
-    // Getting user signed-in status
-    let { user } = $props();
-    $effect(() => {
-        console.log(user);
-    })
+	// Getting user signed-in status
+	let { user } = $props();
+	$effect(() => {
+		console.log(user);
+	});
 </script>
 
-<nav class="flex justify-between items-center p-4 bg-primary-foreground text-primary-background">
-    <div>
-        <a href="/">Home</a>
-        <a href="/forest">Forest</a>
-        <a href="/gallery">Gallery</a>  
-        <a href="/about">About</a>
-    </div>
-            <div class="flex items-center space-x-4">
-                {#if user}
-                <form
-                method="POST"
-                action="/auth/logout"
-                >
-                    <Button type="submit">Log Out</Button>
-                </form>
-            <Avatar.Root>
-                <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-                <Avatar.Fallback>CN</Avatar.Fallback>
-            </Avatar.Root>
-        {:else}
-            <Button href="/auth/login">Login</Button>
-            <Button href="/auth/signup">Signup</Button>
-        {/if}
-    </div>
+<nav class="text-primary-background flex items-center justify-between bg-primary-foreground p-4">
+	<div>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+	</div>
+	<div class="flex items-center space-x-4">
+		{#if user}
+			<form method="POST" action="/auth/logout">
+				<Button type="submit">Log Out</Button>
+			</form>
+			<Avatar.Root>
+				<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+				<Avatar.Fallback>CN</Avatar.Fallback>
+			</Avatar.Root>
+		{:else}
+			<Button href="/auth/login">Login</Button>
+			<Button href="/auth/signup">Signup</Button>
+		{/if}
+	</div>
 </nav>
-
-<style>
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 1rem;
-        background-color: #f0f0f0;
-    }
-
-    nav a {
-        margin-right: 1.5rem;
-        text-decoration: none;
-        color: #333;
-    }
-    
-</style>
