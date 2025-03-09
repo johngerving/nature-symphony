3adbf073857358aea5cd904f0b43b3927bb4e95e
Andrew <gallimoreandrew04@gmail.com>
2025-03-09 10:45:52 -0700

    Added new search ui and filters

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,84 +1,26 @@
 <script lang="ts">
-	import '../app.css';
-	import { invalidate } from '$app/navigation';
-	import { onMount } from 'svelte';
-	import { Navbar } from '$lib/components/ui/navbar';
+    import '../app.css';
+    import { invalidate } from '$app/navigation';
+    import { onMount } from 'svelte';
+    import { Navbar } from '$lib/components/ui/navbar';
+    import { SearchbarResults } from '$lib/components/searchbarResults';
 
-	let { data, children } = $props();
-	let { user, session, supabase } = $derived(data);
+    let { data, children } = $props();
+    let { user, session, supabase } = $derived(data);
 
-	onMount(() => {
-		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
-			if (newSession?.expires_at !== session?.expires_at) {
-				invalidate('supabase:auth');
-			}
-		});
+    onMount(() => {
+        const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+            if (newSession?.expires_at !== session?.expires_at) {
+                invalidate('supabase:auth');
+            }
+        });
 
-		return () => data.subscription.unsubscribe();
-	});
+        return () => data.subscription.unsubscribe();
+    });
 </script>
 
 <Navbar {user}/>
 
-<header>
-
-    <div class="search-bar">
-
-      <input id="search" type="search" placeholder="&#x1F50D; Start typing to search..." />
-
-    </div>
-
-</header>
-
-<main class="container">
-
-    <div class="search-display"></div>
-
-    
-
-    <div class="posts-container"></div>
-
-</main>
-
-<style>
-
-.search-bar {
-
-display: flex;
-
-justify-content: center;
-
-padding: 60px;
-
-}
-
-.search-bar input {
-
-width: 50%;
-
-min-width: 300px;
-
-padding: 12px 24px;
-
-border-radius: 24px;
-
-font-size: 16px;
-
-border: 0px;
-
-outline: none;
-
-}
-
-.search-bar [list]::-webkit-list-button,
-
-.search-bar [list]::-webkit-calendar-picker-indicator {
-
-display: none !important;
-
-}
-
-</style>
-
+<SearchbarResults/>
 
 {@render children()}
